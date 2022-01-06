b1a97fb2e40e45f89fb235cea2e80ac60debb0f9
Lee Attwater <leeattwater@gmail.com>
2022-01-06 19:30:59 +0000

    navbar start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
+import Navbar2 from './components/Navbar2'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Navbar2 />
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  theme: {
+    extend: {
+    },
+  },
+  
+  variants: {
+  },
+  plugins: [],
+}

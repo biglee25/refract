df4e65b99f77397e71d05a5812b7d33aa2abb669
Lee Attwater <leeattwater@gmail.com>
2022-06-15 19:29:25 +0100

    hero

--- components/Hero.js
+++ components/Hero.js
@@ -3,16 +3,16 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-<header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+<header className="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
     <Image src="/images/logo.svg" width={500} height={500} className="text-white z-50" />
-  <div class="z-50 relative p-5 text-6xl font-black text-white">
+  <div className="z-50 relative p-5 text-6xl font-black text-white">
     REFRACTVFX
   </div>
   <video
     autoPlay
     loop
     muted
-    class="absolute z-1 w-auto min-w-full min-h-full max-w-none"
+    className="absolute z-1 w-auto min-w-full min-h-full max-w-none"
   >
     <source
       src="/images/video.mp4"

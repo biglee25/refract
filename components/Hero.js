f2143fb8c52c22e4039e628a3195ba03dcf6d6e1
Lee Attwater <leeattwater@gmail.com>
2022-06-26 16:46:42 +0100

    hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,26 +1,39 @@
 import Image from 'next/image'
+import logo from '../public/images/logo.svg'
 
 export default function Home() {
   return (
     <div>
-<header className="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
-    <Image src="/images/logo.svg" width={500} height={500} className="text-white z-50" />
-  <div className="z-50 relative p-5 text-6xl font-black text-white">
-    REFRACTVFX
-  </div>
-  <video
-    autoPlay
-    loop
-    muted
-    className="absolute z-1 w-auto min-w-full min-h-full max-w-none"
-  >
+<header className="relative flex flex-col items-center justify-center h-screen mb-12 overflow-hidden">
+    <div>
+    <Image
+            src={logo}
+            alt="Refract VFX"
+            width={700}
+            height={520}
+            layout="fixed"
+            className='text-white z-30 blur-md animate-pulse'
+            />
+    </div>
+    <div className="relative flex items-center justify-center p-5 text-6xl font-black text-white z-30">
+            REFRACT<span className='text-red-500'>VFX</span>
+    </div>
+
+    <div className="px-5 text-center text-1xl font-light text-white z-30 md:w-1/2">
+            Delivering perfectly crafted video edits that will make you stand out from the crowd and get you noticed.
+    </div>
+    <video
+        autoPlay
+        loop
+        muted
+        className="absolute w-auto min-w-full min-h-full max-w-none"
+    >
     <source
-      src="/images/video.mp4"
-      type="video/mp4"
-      autoPlay
+    src="/images/video.mp4"
+    type="video/mp4"
+    autoPlay
     />
-    Your browser does not support the video tag.
-  </video>
+    </video>
 </header>
 </div>
   )
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+
+import logo from "../public/images/logo.svg"
+
 import Link from 'next/link'
 
 import { useState} from 'react'
@@ -12,9 +16,20 @@ export default function Navbar() {
 
     return (
         <>
-            <div className="fixed top-0 w-screen h-24 bg-gray-800 flex items-center justify-between px-12 z-50">
+            <div className="fixed w-screen h-24 bg-black flex items-center justify-between z-50">
                 <div>
-                    <h2 className="text-3xl font-bold text-white">REFRACT</h2>
+                    <Link href="/">
+                        <a className="flex flex-row items-center">
+                            <Image
+                            src={logo}
+                            alt="Refract VFX"
+                            width={125}
+                            height={80}
+                            layout="fixed"
+                            />
+                          <h2 className="text-2xl font-black text-white">REFRACT<span className="text-red-500">VFX</span></h2>
+                        </a>
+                    </Link>
                 </div>
                 <ul className="w-auto hidden md:items-center md:justify-around md:flex ml-auto text-white">
                     <li className="px-4">
@@ -38,14 +53,14 @@ export default function Navbar() {
                         </Link>
                     </li>
                 </ul>
-                <button onClick={() => setOpen(!open)} className="text-white cursor-pointer ml-auto md:hidden z-40" type="button">
+                <button onClick={() => setOpen(!open)} className="text-white cursor-pointer ml-auto px-5 md:hidden" type="button">
                     <Hamburger toggled={open} toggle={setOpen} />
                 </button>
             </div>
-            <div className="overflow-hidden fixed md:hidden top-0 left-0 right-0">
-                <div className={(open ? "z-30 flex translate-y-100 transition ease-out duration-500 overflow-y-hidden" : "translate-y-full transition ease-out duration-500")}>
-                    <div className="w-full h-screen bg-gray-800 text-white flex flex-col items-center justify-center overflow-y-hidden">
-                        <ul className="text-center font-bold text-4xl leading-loose overflow-y-hidden">
+            <div className="overflow-y-hidden fixed md:hidden absolute top-0 left-0 right-0">
+                <div className={(open ? "flex opacity-100" : "opacity-0")}>
+                    <div className="w-full h-screen fixed overflow-y-hidden bg-gray-800 text-white flex flex-col items-center justify-center z-40">
+                        <ul className="text-center font-bold text-4xl leading-loose">
                             <li className="px-4">
                                 <Link href="/">
                                     <a className="hover:text-orange-400" onClick={() => setTimeout(() => {setOpen(false)}, 150)}>Home</a>

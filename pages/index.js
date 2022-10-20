9b06bfdb259b5bce24fb67c5b34d707c8aba9bab
Lee Attwater <leeattwater@gmail.com>
2022-10-20 18:51:03 +0100

    new

--- pages/index.js
+++ pages/index.js
@@ -30,25 +30,6 @@ export default function Home({ data }) {
       <About />
       <Card />
       <Work />
-      {/* <div className="container mx-auto my-12">
-        <h1 className="text-center text-6xl font-bold py-12 text-black mx-auto">
-          Featured Work
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-
-          {/* {data.items.map(({ id, snippet = {} }) => {
-                const { title, thumbnails = {}, resourceId = {} } = snippet;
-                const { medium = {} } = thumbnails;
-                return (
-              <div key={id} className="m-4 p-4 shadow-md h-full rounded-lg bg-gray-900 text-white z-30">
-                <a href={`https://www.youtube.com/watch?v=${resourceId.videoId}`} target="_blank" rel="noreferrer">
-                    <img width={medium.width} height={medium.height} src={medium.url} alt="Youtube Image" className="w-full" />
-                    <h3 className="text-center pt-8">{ title }</h3>
-                </a>
-              </div>
-          )})} */}
-        </div>
-      </div> */}
       <Footer />  
     </>
   )

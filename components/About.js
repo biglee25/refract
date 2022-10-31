1ca6e8197845843b84a11e52be2cdfb067f58589
Lee Attwater <leeattwater@gmail.com>
2022-10-31 19:09:09 +0000

    hero change

--- components/About.js
+++ components/About.js
@@ -1,25 +1,21 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import logo from '../public/images/video-edit.jpg'
-
 
 export default function About() {
   return (
     <div>
-        <section className="container mx-auto m-4 md:mx-12 md:my-48">
-        <h2 className='pt-12 px-5 text-blue-300 text-5xl md:text-8xl'>A little about me....</h2>
-            <div className="my-12 px-5 grid grid-cols-1 md:grid-cols-3 gap-12">
-                <div>
-                    <p>My own personal venture into the world of post production has improved my skills and understanding of what it takes to produce an eye catching piece of work. I have been using applications like Adobe Premiere Pro and Adobe After Effects for over 2 years and have found them to have been crucial in my development in post production. Developing my skills through such outlets has helped me to grow a sense of perfectionism for the work I have created.</p>
-                </div>
-                <div>
-                    <p>Working as a freelance video editor in my spare time shows I am committed and passionate for the post production industry and am always willing to know how I can better my work. I have used my criticism of post production to look at it as more of an art than a form of entertainment. Freelance video editing helps me gain lots of professionalism and I always enjoy doing it as I would say it is one of my hobbies.</p>
-                </div>
-                <div>
-                <p> During the past 2 years of video editing I have worked with a number of different clients who have always been impressed with the work I have done for them. Some clients I have worked with have been kind enough to give me working references for future opportunities that may come my way.</p>
-                </div>
-            </div>
-        </section>
+          <div className="container mx-auto m-4 md:mx-12 md:my-48 logo-bg text-black">
+            <h2 className='pt-12 px-5 text-blue-300 text-5xl md:text-8xl'>A little about me....</h2>
+              <div className="my-12 px-5 grid grid-cols-1 md:grid-cols-3 gap-12">
+                  <div>
+                      <p>My own personal venture into the world of post production has improved my skills and understanding of what it takes to produce an eye catching piece of work. I have been using applications like Adobe Premiere Pro and Adobe After Effects for over 2 years and have found them to have been crucial in my development in post production. Developing my skills through such outlets has helped me to grow a sense of perfectionism for the work I have created.</p>
+                  </div>
+                  <div>
+                      <p>Working as a freelance video editor in my spare time shows I am committed and passionate for the post production industry and am always willing to know how I can better my work. I have used my criticism of post production to look at it as more of an art than a form of entertainment. Freelance video editing helps me gain lots of professionalism and I always enjoy doing it as I would say it is one of my hobbies.</p>
+                  </div>
+                  <div>
+                  <p> During the past 2 years of video editing I have worked with a number of different clients who have always been impressed with the work I have done for them. Some clients I have worked with have been kind enough to give me working references for future opportunities that may come my way.</p>
+                  </div>
+              </div>
+          </div>
     </div>
   )
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,10 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import logo from '../public/images/profile.png'
-
 export default function Home() {
   return (
     <div>
         <section className='grid grid-cols-1 md:grid-cols-2 mt-12 md:mt-12 bg-black bg-hero-bg bg-contain bg-no-repeat bg-top-right md:bg-right h-screen'>
-						<div className='flex flex-col items-center justify-center p-4'>
+						<div className='flex flex-col items-center justify-center p-4 md:p-12'>
 							<div className='text-left mx-auto'>
-								<h2 className='text-blue-300 font-extralight text-8xl md:text-9xl mb-24'>PORTFOLIO</h2>
+								<h2 className='text-white font-extralight text-6xl md:text-9xl mb-24'>PORTFOLIO</h2>
 								<h2 className='text-white text-5xl md:text-8xl'>JACK ATTWATER</h2>
 								<h1 className='text-black font-extralight bg-blue-300 p-8 mt-24 w-max'>VIDEO EDITOR</h1>
 							</div>

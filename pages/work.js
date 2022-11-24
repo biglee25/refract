8f6bbbf2471ae898f5518b02440dfc52ad9a344e
Lee Attwater <leeattwater@gmail.com>
2022-11-24 18:30:45 +0000

    more content

--- pages/work.js
+++ pages/work.js
@@ -7,9 +7,9 @@ export default function Work() {
         <h2 className='px-5 text-blue-300 text-5xl md:text-8xl text-center'>My Work</h2>
             <p className='text-black px-5 py-6 md:w-3/4 mx-auto text-center'>Below you will find examples of my work. Please click on a video to see the full brief.</p>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-                <div className="flex flex-col items-center rounded-lg cursor-pointer">
+                <div className="flex flex-col items-center cursor-pointer">
                     <Link href="/adidas">
-                        <div className="bg-adidas-bg bg-no-repeat bg-cover bg-center opacity-100 hover:opacity-80 text-white w-full h-96 flex items-start justify-start p-5">
+                        <div className="bg-adidas-bg bg-no-repeat bg-cover bg-center text-white w-full h-96 flex items-start justify-start p-5">
                             <a>
                                 <h2>Adidas Motion Graphic</h2>
                             </a>
@@ -18,7 +18,7 @@ export default function Work() {
                 </div>
                 <div className="flex flex-col items-center rounded-lg cursor-pointer">
                     <Link href="/nike">
-                        <div className="bg-nike-bg bg-no-repeat bg-cover bg-center opacity-100 hover:opacity-80 text-white w-full h-96 flex items-start justify-start p-5">
+                        <div className="bg-nike-bg bg-no-repeat bg-cover bg-center text-white w-full h-96 flex items-start justify-start p-5">
                             <a>
                                 <h2>Nike Motion Graphic</h2>
                             </a>
@@ -27,7 +27,7 @@ export default function Work() {
                 </div>
                 <div className="flex flex-col items-center rounded-lg cursor-pointer">
                     <Link href="/fight-for-promotion">
-                        <div className="bg-ffp-bg bg-no-repeat bg-cover bg-center opacity-100 hover:opacity-80 text-black w-full h-96 flex items-start justify-start p-5">
+                        <div className="bg-ffp-bg bg-no-repeat bg-cover bg-center text-black w-full h-96 flex items-start justify-start p-5">
                             <a>
                                 <h2>Fight For Promotion</h2>
                             </a>
@@ -36,7 +36,7 @@ export default function Work() {
                 </div>
                 <div className="flex flex-col items-center rounded-lg cursor-pointer">
                 <Link href="/stay">
-                        <div className="bg-stay-bg bg-no-repeat bg-cover bg-center opacity-100 hover:opacity-80 text-black w-full h-96 flex items-start justify-start p-5">
+                        <div className="bg-stay-bg bg-no-repeat bg-cover bg-center text-black w-full h-96 flex items-start justify-start p-5">
                             <a>
                                 <h2>STAY</h2>
                             </a>
